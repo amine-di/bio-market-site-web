cb249ca681e4b4ea4881f75a30e3cb7dc4913c2e
Amine <amine.aboutaieb@gmail.com>
2020-06-02 22:27:01 +0100

    Confirmation compte avant login

--- App/login_compte.php
+++ App/login_compte.php
@@ -2,27 +2,30 @@
 session_start();
 require('../db/fonctions_db.php');
 
-if(isset($_POST['loginBtn'])){
+if (isset($_POST['loginBtn'])) {
 
     $email = $_POST['email'];
     $pwd = $_POST['pwd'];
 
-    if(!empty($email) && !empty($pwd)){
 
-        $result = loginClient($email,$pwd);
-        if($result == 1){
-            $_SESSION['email'] = $email;
-            header('Location: /bio_market/index.php');
-        }else{
+    if (!empty($email) && !empty($pwd)) {
+
+        $result = loginClient($email, $pwd);
+
+        if ($result == 1) {
+            $statut = verifierStatut($email);
+            if ($statut['statut'] == 1) {
+                $_SESSION['email'] = $email;
+                header('Location: /bio_market/index.php');
+            } else {
+                header('Location: /bio_market/views/Error404.php?errorMsg=Ce compte n\'a pas été validé');
+            }
+        } else {
             header('Location: /bio_market/views/Error404.php?errorMsg=Ce compte n\'existe pas');
         }
-
-    }else{
-        header('Location: bio_market/views/Error404.php?errorMsg=Veuillez remplir tous les champs SVP');
+    } else {
+        header('Location: /bio_market/views/Error404.php?errorMsg=Veuillez remplir tous les champs SVP');
     }
-
-}else{
+} else {
     header('Location: /bio_market/index.php');
 }
-?>
-
--- index.php
+++ index.php
@@ -5,6 +5,7 @@ $categories = getAllCategories();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,19 +13,20 @@ $categories = getAllCategories();
     <link rel="stylesheet" href="http://192.168.1.4/bio_market/css/global.css">
     <link rel="stylesheet" href="http://192.168.1.4/bio_market/css/home.css">
 </head>
+
 <body>
-    <?php require('public/header.php');?>
+    <?php require('public/header.php'); ?>
     <div id="backImg"></div>
     <h2 id="catPageTitle">Nos categories</h2>
     <div id="categorieListe">
         <?php
-            foreach($categories as $categorie){
-                echo "<div class='cat'>";
-                    echo "<h2 class='catTitle'>".$categorie['nom']."</h2>";
-                    $img = "data:image/png;base64,".base64_encode($categorie['cat_img'])."";
-                    echo "<div style='background-image:url(".$img.");' class='catImg' id=".$categorie['id']."></div>";
-                echo "</div>";
-            }
+        foreach ($categories as $categorie) {
+            echo "<div class='cat'>";
+            echo "<h2 class='catTitle'>" . $categorie['nom'] . "</h2>";
+            $img = "data:image/png;base64," . base64_encode($categorie['cat_img']) . "";
+            echo "<div style='background-image:url(" . $img . ");' class='catImg' id=" . $categorie['id'] . "></div>";
+            echo "</div>";
+        }
         ?>
         <!--
         <div class="cat">
@@ -36,4 +38,5 @@ $categories = getAllCategories();
     <?php require('public/session_bar.php'); ?>
     <script src="http://192.168.1.4/bio_market/scripts/index.js"></script>
 </body>
+
 </html>
